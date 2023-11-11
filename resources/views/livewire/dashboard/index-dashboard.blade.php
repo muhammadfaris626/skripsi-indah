216214bed110f258ec1533cb5eb2de4698eb2101
muhammadfaris626 <muhammadfaris5795@gmail.com>
2023-11-11 19:17:38 +0800

    update

--- resources/views/livewire/dashboard/index-dashboard.blade.php
+++ resources/views/livewire/dashboard/index-dashboard.blade.php
@@ -7,7 +7,7 @@
                 <section class="text-gray-600 body-font overflow-hidden">
                     <div class="container px-5 pb-10 mx-auto">
                         <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                            <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="{{ asset($newProduct->product_image[0]) }}">
+                            <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="{{ asset("storage/".$newProduct->product_image[0]) }}">
                             <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                             <h2 class="text-sm title-font text-gray-500 tracking-widest">{{ Str::upper($newProduct->brand->name) }}</h2>
                             <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{ Str::upper($newProduct->name) }}</h1>
@@ -63,77 +63,29 @@
             @if(count($products) == 0)
             @else
                 <section class="text-gray-600 body-font">
-                    <h1 class="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 dark:text-white">Products</h1>
-                    <div class="container px-5 mx-auto">
-                        <div class="flex flex-wrap -m-4">
+                    <div class="container px-5 py-24 mx-auto">
+                        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
                             @foreach ($products as $v)
-                                <a href="{{ route('detail-product', $v->id) }}">
-                                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                                        <p class="block relative rounded overflow-hidden">
-                                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset("storage/".$v->product_image[0]) }}">
-                                        </p>
-                                        <div class="mt-4">
-                                            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ Str::upper($v->brand->name) }}</h3>
-                                            <h2 class="text-gray-900 title-font text-lg font-medium">{{ Str::upper($v->name) }}</h2>
-                                        </div>
+                                <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+                                    <div class="rounded-lg h-64 overflow-hidden">
+                                        <img alt="content" class="object-cover object-center h-full w-full" src="{{ asset("storage/".$v->product_image[0]) }}">
                                     </div>
-                                </a>
+                                    <div class="mt-4">
+                                        <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ Str::upper($v->brand->name) }}</h3>
+                                        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">{{ Str::upper($v->name) }}</h2>
+                                    </div>
+                                    <a class="text-indigo-500 inline-flex items-center mt-3" href="{{ route('detail-product', $v->id) }}">LIHAT
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                        </svg>
+                                    </a>
+                                </div>
                             @endforeach
                         </div>
                     </div>
                 </section>
             @endif
-            {{-- <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="flex flex-col">
-                        <div class="h-1 bg-gray-200 rounded overflow-hidden">
-                        <div class="w-24 h-full bg-indigo-500"></div>
-                        </div>
-                        <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
-                        <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">Space The Final Frontier</h1>
-                        <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
-                        </div>
-                    </div>
-                    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-                        <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-                        <div class="rounded-lg h-64 overflow-hidden">
-                            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1203x503">
-                        </div>
-                        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">Shooting Stars</h2>
-                        <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-                        <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                            </svg>
-                        </a>
-                        </div>
-                        <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-                        <div class="rounded-lg h-64 overflow-hidden">
-                            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504">
-                        </div>
-                        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">The Catalyzer</h2>
-                        <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-                        <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                            </svg>
-                        </a>
-                        </div>
-                        <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-                        <div class="rounded-lg h-64 overflow-hidden">
-                            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505">
-                        </div>
-                        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">The 400 Blows</h2>
-                        <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-                        <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                            </svg>
-                        </a>
-                        </div>
-                    </div>
-                </div>
-            </section> --}}
+
         </div>
     </div>
 </div>
